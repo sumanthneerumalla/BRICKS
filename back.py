ec5da0def9fbce22a8f6f503a49ac8e11246f1f8
Zach Elliott <elliottzach86@gmail.com>
2017-11-13 13:46:55 -0500

    Back end algorithm now creates districts and displays population.

--- back.py
+++ back.py
@@ -1,10 +1,14 @@
+'''back.py'''
 import csv
+
 
 # Number of districts to partition
 NUM_DISTRICTS = 8
 
-# Open CSV file that has all MD ZIP codes
-data = open('data_filtered.csv', 'rt')
+# Dictionary to store all of the ZIP codes as keys and their populations as values
+# Key = String - ZIP code
+# Value = Int - Population of ZIP code
+pops = {}
 
 # Dictionary to store all of the ZIP codes as keys and boundary coordinates as values
 # Key = String - ZIP code
@@ -16,58 +20,144 @@ zips = {}
 # Value = String Array - List of neighboring ZIP codes
 neighbors = {}
 
+# Dictionary to store all of the districts and the zip codes within them
+# Key = String - District name
+# Value = String Array - List of zip codes in district
+districts = {}
+
 # Keep track of the overall population
 total_population = 0
 
-# Read in all of the ZIP codes and their boundaries
-for row in csv.reader(data):
-	coordinates = row[8]
-	cord_list = coordinates.split(' ')
-	zips[row[1]] = cord_list
-	total_population += row[3]
-
 # Store the number of people that should be in each district
-district_population = total_population / NUM_DISTRICTS
+pop_per_district = 0
 
-# Iterate over the dictionary of ZIPs
-for key, value in zips.items():
 
-	neighbor_set = set()
+def taken(zip_code):
+	''' Returns True if ZIP code is already taken by another district
+		and False if the ZIP code is available '''
+
+	for key, value in districts.items():
+		if zip_code in value:
+			return True
+
+	return False
+
+
+def read_data():
+	''' Reads the data (ZIP codes, boundary coordinates, populations)
+		into their corresponding data structures '''
+
+	global total_population
+
+	# Open CSV file that has all MD ZIP codes
+	data = open('data_filtered.csv', 'rt')
+
+	# Read in all of the ZIP codes and their boundaries
+	for row in csv.reader(data):
+		coordinates = row[8]
+		cord_list = coordinates.split(' ')
+		zips[row[1]] = cord_list
+		pops[row[1]] = int(row[3])
+		total_population += int(row[3])
+
+
+def find_neighbors():
+	''' Iterates through all of the ZIP codes stores neighbors
+		when two ZIP codes share a boundary '''
 
 	# Iterate over the dictionary of ZIPs
-	for key2, value2 in zips.items():
+	for key, value in zips.items():
+		neighbor_set = set()
 
-		# If you're comparing the same zip code, skip it
-		if key == key2:
-			continue
+		# Iterate over the dictionary of ZIPs
+		for key2, value2 in zips.items():
 
-		# Iterate over the first list of coordinates
-		for item in value:
+			# If you're comparing the same zip code, skip it
+			if key == key2:
+				continue
 
-			# Iterate over the second list of coordinates
-			for item2 in value2:
+			# Iterate over the first list of coordinates
+			for item in value:
 
-				# If the coordinates are the same, the ZIPs are neighbors
-				if item == item2:
-					neighbor_set.add(key2)
+				# Iterate over the second list of coordinates
+				for item2 in value2:
 
-	neighbors[key] = neighbor_set
+					# If the coordinates are the same, the ZIPs are neighbors
+					if item == item2:
+						neighbor_set.add(key2)
 
-for key, value in neighbors.items():
-	print(key, ':', value)
+		neighbors[key] = neighbor_set
 
 
+def create_districts():
+	''' Creates contiguous districts of relatively equal size
+		and stores them in a dictionary '''
 
-class Zip:
-	def __init__(self, name, neighbors):
-		self.name = name
-		self.neighbors = neighbors
-		self.taken = false
+	# Iterate over the districts
+	for district, value1 in districts.items():
+		current_pop = 0
 
-class District:
-	def __init__(self, number):
-		self.number = number
-		self.zips = Set()
+		# Iterate over the neighbor dictionary
+		for neighbor, value2 in neighbors.items():
+			
+			# Only add ZIP codes while the districts population is not high enough
+			if current_pop < pop_per_district:
 
-	def addZip(self, zip):
-		self.zips.add(zip)
+				# If the key ZIP code is not taken, add it to the district
+				if not taken(neighbor):
+					value1.add(neighbor)
+					current_pop += pops[neighbor]
+
+			# Iterate over the neighbors of the current ZIP
+			for i in value2:
+
+				# Only add ZIP codes while the districts population is not high enough
+				if current_pop < pop_per_district:
+
+					# If ZIP code is not taken, add it to the district
+					if not taken(i):
+						value1.add(i)
+						current_pop += pops[i]
+
+
+def print_dictionary(dictionary):
+	''' Print out the passed in dictionary in the form:
+		key : value '''
+
+	for key, value in dictionary.items():
+		print(key, ':', value)
+		print()
+
+
+def print_district_pops():
+	''' Print out the population of each district '''
+
+	for key, value in districts.items():
+		district_pop = 0
+
+		for i in value:
+			district_pop += pops.get(i)
+
+		print(key, 'population:', district_pop)
+
+
+# Read in data from input file
+read_data()
+
+pop_per_district = total_population / NUM_DISTRICTS
+
+# Find all of the neighbors of each ZIP code
+find_neighbors()
+
+#print_dictionary(neighbors)
+
+# Fill dictionary with correct number of districts and empty zip code list
+for x in range(NUM_DISTRICTS):
+	districts['District ' + str(x+1)] = set()
+
+# Create the districts
+create_districts()
+
+print_dictionary(districts)
+
+print_district_pops()
