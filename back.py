cfe29376679c1f03626cc7853484db58976fea60
Zach Elliott <elliottzach86@gmail.com>
2017-11-29 21:19:47 -0500

    Made significant improvements to districting algorithm.

--- back.py
+++ back.py
@@ -1,5 +1,7 @@
 '''back.py'''
 import csv
+import copy
+from geopy.distance import vincenty
 
 # State to district
 STATE = 'MD'
@@ -27,22 +29,16 @@ neighbors = {}
 # Value = String Array - List of zip codes in district
 districts = {}
 
+# Dictionary to store all of the ZIP codes as keys and the coordinates of their center point as values
+# Key = String - ZIP code
+# Value = Float Tuple - Pair of coordinates
+centers = {}
+
 # Keep track of the overall population
 total_population = 0
 
 # Store the number of people that should be in each district
 pop_per_district = 0
-
-
-def taken(zip_code):
-	''' Returns True if ZIP code is already taken by another district
-		and False if the ZIP code is available '''
-
-	for key, value in districts.items():
-		if zip_code in value:
-			return True
-
-	return False
 
 
 def read_data():
@@ -58,14 +54,16 @@ def read_data():
 	for row in csv.reader(data):
 		coordinates = row[8]
 		cord_list = coordinates.split(' ')
-		
+
 		# Make sire there are no blank coordinates
 		for item in cord_list:
 			if item == '':
 				cord_list.remove(item)
-		
+
 		zips[row[1]] = cord_list
 		pops[row[1]] = int(row[3])
+		center_tuple = (float(row[2]), float(row[6]))
+		centers[row[1]] = center_tuple
 		total_population += int(row[3])
 
 
@@ -75,7 +73,7 @@ def find_neighbors():
 
 	# Iterate over the dictionary of ZIPs
 	for key, value in zips.items():
-		neighbor_set = set()
+		neighbor_list = []
 
 		# Iterate over the dictionary of ZIPs
 		for key2, value2 in zips.items():
@@ -92,45 +90,106 @@ def find_neighbors():
 
 					# If the coordinates are the same, the ZIPs are neighbors
 					if item == item2:
-						neighbor_set.add(key2)
 
-		neighbors[key] = neighbor_set
+						# Don't add the same zips twice
+						if key2 not in neighbor_list:
+							neighbor_list.append(key2)
+
+		neighbors[key] = neighbor_list
+
+
+def find_closest_neighbor(zip_code, taken, current_neighbors):
+	'''Returns the closest zip code to the zip passed in that
+		is not already taken'''
+
+	potential_list = []
+	zip_center = centers.get(zip_code)
+
+	# Add all zip codes that are not taken to the potential list
+	for item in centers.keys():
+		if item not in taken and item not in current_neighbors:
+			potential_list.append(item)
+
+	closest = potential_list[0]
+
+	# Loop through the potential list and find the zip with the
+	# smallest distance to our target zip code
+	for item in potential_list:
+		closest_center = centers.get(closest)
+		test_center = centers.get(item)
+
+		if vincenty(zip_center, closest_center).m > vincenty(zip_center, test_center).m:
+			closest = item
+
+	return closest
 
 
 def create_districts():
 	''' Creates contiguous districts of relatively equal size
 		and stores them in a dictionary '''
 
-	# Iterate over the districts
-	for district, value1 in districts.items():
-		current_pop = 0
+	current_neighbors = []
+	taken = []
+	district_list = []
+	district_count = 1
+	current_pop = 0
+	first_iteration = True
 
-		# Iterate over the neighbor dictionary
-		for neighbor, value2 in neighbors.items():
-			
-			# Only add ZIP codes while the districts population is not high enough
-			if current_pop < pop_per_district:
+	while len(taken) != len(zips.keys()):
 
-				# If the key ZIP code is not taken, add it to the district
-				if not taken(neighbor):
-					value1.add(neighbor)
-					current_pop += pops[neighbor]
+		# If the current population becomes larger than the population per district,
+		# save the current district and start the next one
+		if current_pop >= pop_per_district:
+			districts['District ' + str(district_count)] = district_list
+			district_list = []
+			district_count += 1
+			current_pop = 0
 
-			# Iterate over the neighbors of the current ZIP
-			for i in value2:
+		if first_iteration:
+			first_iteration = False
+			current_zip = list(neighbors.keys())[0]
+			district_list.append(current_zip)
+			taken.append(current_zip)
+			current_pop += pops.get(current_zip)
+			n_list = neighbors.get(current_zip)
 
-				# Only add ZIP codes while the districts population is not high enough
-				if current_pop < pop_per_district:
+			for item in n_list:
+				if item not in taken and item not in current_neighbors:
+					current_neighbors.append(item)
 
-					# If ZIP code is not taken, add it to the district
-					if not taken(i):
-						value1.add(i)
-						current_pop += pops[i]
+		else:
+
+			# If we're on the last zip code, add it to the last district and exit loop
+			if len(taken) == len(zips.keys()) - 1:
+				district_list.append(current_zip)
+				districts['District ' + str(district_count)] = district_list
+				break
+
+			if not current_neighbors:
+				current_zip = find_closest_neighbor(current_zip, taken, current_neighbors)
+				n_list = neighbors.get(current_zip)
+
+				for item in n_list:
+					if item not in taken and item not in current_neighbors:
+						current_neighbors.append(item)
+
+			else:
+				current_zip = current_neighbors[0]
+				current_neighbors.remove(current_zip)
+				district_list.append(current_zip)
+				taken.append(current_zip)
+				current_pop += pops.get(current_zip)
+				n_list = neighbors.get(current_zip)
+
+				for item in n_list:
+					if item not in taken and item not in current_neighbors:
+						current_neighbors.append(item)
 
 
 def print_dictionary(dictionary):
 	''' Print out the passed in dictionary in the form:
 		key : value '''
+
 	values = 0
 
 	for key, value in dictionary.items():
@@ -141,6 +200,7 @@ def print_dictionary(dictionary):
 
 def print_district_pops():
 	''' Print out the population of each district '''
+
 	sum = 0
 	for key, value in districts.items():
 		district_pop = 0
@@ -148,6 +208,7 @@ def print_district_pops():
 		for i in value:
 			district_pop += pops.get(i)
 			sum += pops.get(i)
+
 		print(key, 'population:', district_pop)
 
 	print("Sum:", sum)
@@ -160,10 +221,6 @@ pop_per_district = total_population / NUM_DISTRICTS
 # Find all of the neighbors of each ZIP code
 find_neighbors()
 
-# Fill dictionary with correct number of districts and empty zip code list
-for x in range(NUM_DISTRICTS):
-	districts['District ' + str(x+1)] = set()
-
 # Create the districts
 create_districts()
 
@@ -171,7 +228,6 @@ print_dictionary(districts)
 
 print_district_pops()
 
-'''
 # Open output file for writing HTML script
 file2 = open('output.txt', 'wt')
 
@@ -190,7 +246,7 @@ for dist11, zip11 in districts.items():
 		stringEnd = '\tstrokeColor: "' + str(strokeColors[outerCount]) + '",\n\tstrokeOpacity: 1.0,\n\tstrokeWeight: 2,\n\tmap: map\n});'
 
 		for key2, value2 in zips.items():
-			if x == key2: 
+			if x == key2:
 				for i in value2:
 					crd_lst = i.split(',')
 					new_str = string2 + crd_lst[1] + ',' + crd_lst[0] + '),'
@@ -208,7 +264,7 @@ for dist11, zip11 in districts.items():
 	outerCount += 1
 
 file2.close()
-'''
 
 # Check to make sure sum of district populations equals the total population
+print("Population per district:", pop_per_district)
 print("Total population:", total_population)
