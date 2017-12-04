7cc30842905fa6cbeffd385776441ec3b0f087b9
Zell86 <elliottzach86@gmail.com>
2017-12-03 19:03:12 -0500

    Updated application to automatically generate and display output

--- back.py
+++ back.py
@@ -1,59 +1,17 @@
 '''back.py'''
 import csv
-import copy
+import json
 from geopy.distance import vincenty
 
-# State to district
-STATE = 'MD'
 
-# Number of districts to partition
-NUM_DISTRICTS = 8
-
-# Dictionary to store all of the ZIP codes as keys and their populations as values
-# Key = String - ZIP code
-# Value = Int - Population of ZIP code
-pops = {}
-
-# Dictionary to store all of the ZIP codes as keys and boundary coordinates as values
-# Key = String - ZIP code
-# Value = String Array - List of coordinate boundaries
-zips = {}
-
-# Dictionary to store all of the ZIP codes as keys and their neighboring ZIP codes as values
-# Key = String - ZIP code
-# Value = String Array - List of neighboring ZIP codes
-neighbors = {}
-
-# Dictionary to store all of the districts and the zip codes within them
-# Key = String - District name
-# Value = String Array - List of zip codes in district
-districts = {}
-
-# Dictionary to store all of the districts and their outlining boundary
-# Key = String - District name
-# Value = String Set - Set of coordinates in the boundary
-district_boundary = {}
-
-# Dictionary to store all of the ZIP codes as keys and the coordinates of their center point as values
-# Key = String - ZIP code
-# Value = Float Tuple - Pair of coordinates
-centers = {}
-
-# Keep track of the overall population
-total_population = 0
-
-# Store the number of people that should be in each district
-pop_per_district = 0
-
-
-def read_data():
+def read_data(state, zips, pops, centers):
 	''' Reads the data (ZIP codes, boundary coordinates, populations)
 		into their corresponding data structures '''
 
-	global total_population
+	total_population = 0
 
 	# Open CSV file that has all MD ZIP codes
-	data = open('Data/data-filtered/data_filtered_' + STATE + '.csv', 'rt')
+	data = open('Data/data-filtered/data_filtered_' + state + '.csv', 'rt')
 
 	# Read in all of the ZIP codes and their boundaries
 	for row in csv.reader(data):
@@ -71,8 +29,10 @@ def read_data():
 		centers[row[1]] = center_tuple
 		total_population += int(row[3])
 
+	return total_population, zips, pops, centers
 
-def find_neighbors():
+
+def find_neighbors(neighbors, zips):
 	''' Iterates through all of the ZIP codes stores neighbors
 		when two ZIP codes share a boundary '''
 
@@ -102,8 +62,10 @@ def find_neighbors():
 
 		neighbors[key] = neighbor_list
 
+	return neighbors, zips
 
-def find_closest_neighbor(zip_code, taken):
+
+def find_closest_neighbor(zip_code, taken, centers):
 	'''Returns the closest zip code to the zip passed in that
 		is not already taken'''
 
@@ -129,7 +91,7 @@ def find_closest_neighbor(zip_code, taken):
 	return closest
 
 
-def create_districts(pop_per_district):
+def create_districts(pop_per_district, districts, zips, neighbors, pops, centers):
 	''' Creates contiguous districts of relatively equal size
 		and stores them in a dictionary '''
 
@@ -144,8 +106,6 @@ def create_districts(pop_per_district):
 
 		# If the current population becomes larger than the population per district,
 		# save the current district and start the next one
-		#print('Length of taken: ' + str(len(taken)))
-		#print('Length of zips: ' + str(len(zips.keys())))
 		if current_pop >= pop_per_district:
 			districts['District ' + str(district_count)] = district_list
 			district_list = []
@@ -176,7 +136,7 @@ def create_districts(pop_per_district):
 				break
 
 			if not current_neighbors:
-				current_zip = find_closest_neighbor(current_zip, taken)
+				current_zip = find_closest_neighbor(current_zip, taken, centers)
 				district_list.append(current_zip)
 				taken.append(current_zip)
 				current_pop += pops.get(current_zip)
@@ -198,66 +158,37 @@ def create_districts(pop_per_district):
 					if item not in taken and item not in current_neighbors:
 						current_neighbors.append(item)
 
+	return districts
+
 
 def print_dictionary(dictionary):
 	''' Print out the passed in dictionary in the form:
 		key : value '''
 
-	values = 0
-
 	for key, value in dictionary.items():
 		print(key, ':', value)
 		print()
-		values += len(value)
 
 
-def print_district_pops():
-	''' Print out the population of each district '''
+def get_district_pops(districts, pops, districts_pops):
+	'''Get the population of each district '''
 
-	sum = 0
+	count = 1
 	for key, value in districts.items():
 		district_pop = 0
 
 		for i in value:
 			district_pop += pops.get(i)
-			sum += pops.get(i)
 
-		print(key, 'population:', district_pop)
+		districts_pops['District ' + str(count)] = district_pop
+		count += 1
 
-	print("Sum:", sum)
-
-
-def outline_districts():
-	'''Union all of the zip code coordinate boundaries for
-		each district to get only the outlining boundary'''
-
-	# Iterate over the district dictionary
-	for district, zip_codes in districts.items():
-
-		current_boundary = []
-		first_iteration = True
-
-		for item in zip_codes:
-
-			if first_iteration:
-				first_iteration = False
-				set1 = zips.get(item)
-				for i in set1:
-					current_boundary.append(i)
-				continue
-
-			set1 = zips.get(item)
-			#print("Curent boundary before: " + str(current_boundary))
-			current_boundary = set(current_boundary).symmetric_difference(set1)
-			#print("Curent boundary after: " + str(current_boundary))
-			#print()
-
-		district_boundary[district] = current_boundary
+	return districts_pops
 
 
-def output_individuals():
+def output_individuals(districts, zips):
 	# Open output file for writing HTML script
-	file2 = open('output.txt', 'wt')
+	file2 = open('output.js', 'wt')
 
 	strokeColors = ['#FF0000', '#0078FF', '#663300', '#ffff00', '#009900', '#660066', '#ff9900', '#ff99ff']
 	outerCount = 0
@@ -294,68 +225,77 @@ def output_individuals():
 	file2.close()
 
 
-def output_outlines():
-	# Open output file for writing HTML script
-	file2 = open('output_copy.txt', 'wt')
-
-	strokeColors = ['#FF0000', '#0078FF', '#663300', '#ffff00', '#009900', '#660066', '#ff9900', '#ff99ff']
-	count =  0
-
-	for district, outline in district_boundary.items():
-
-		string1 = '\tpath: ['
-		string2 = 'new google.maps.LatLng('
-
-		for x in outline:
-			crd_lst = x.split(',')
-			new_str = string2 + crd_lst[1] + ',' + crd_lst[0] + '),'
-			string1 = string1 + new_str
-
-		stringBegin = 'var line' + str(count) + ' = new google.maps.Polyline({\n'
-		stringEnd = '\tstrokeColor: "' + str(strokeColors[count]) + '",\n\tstrokeOpacity: 1.0,\n\tstrokeWeight: 2,\n\tmap: map\n});'
-		count += 1
-
-		string1 = string1[:-1]
-		string1 += '],\n'
-		file2.write(stringBegin + string1 + stringEnd)
-		file2.write('\n')
-		file2.write('\n')
-
-		string1 = 'path: ['
-
-	file2.close()
-
-
-def run(state, num_districts):
+def run(st, num_d):
 	'''Run the program'''
-	
-	STATE = state
-	NUM_DISTRICTS = int(num_districts)
 
-	print('State: ' + state)
-	print('Num_districts: ' + str(num_districts))
+	# Dictionary to store all of the ZIP codes as keys and their populations as values
+	# Key = String - ZIP code
+	# Value = Int - Population of ZIP code
+	pops = {}
+
+	# Dictionary to store all of the ZIP codes as keys and boundary coordinates as values
+	# Key = String - ZIP code
+	# Value = String Array - List of coordinate boundaries
+	zips = {}
+
+	# Dictionary to store all of the ZIP codes as keys and their neighboring ZIP codes as values
+	# Key = String - ZIP code
+	# Value = String Array - List of neighboring ZIP codes
+	neighbors = {}
+
+	# Dictionary to store all of the districts and the zip codes within them
+	# Key = String - District name
+	# Value = String Array - List of zip codes in district
+	districts = {}
+
+	# Dictionary to store all of the districts and their total populations
+	# Key = String - District name
+	# Value = Integer - Total population of the district
+	districts_pops = {}
+
+	# Dictionary to store all of the districts and their outlining boundary
+	# Key = String - District name
+	# Value = String Set - Set of coordinates in the boundary
+	district_boundary = {}
+
+	# Dictionary to store all of the ZIP codes as keys and the coordinates of their center point as values
+	# Key = String - ZIP code
+	# Value = Float Tuple - Pair of coordinates
+	centers = {}
+
+	# Keep track of the overall population
+	total_population = 0
+
+	# Store the number of people that should be in each district
+	pop_per_district = 0
+
+	state = st
+	num_districts = int(num_d)
 
 	# Read in data from input file
-	read_data()
+	total_population, zips, pops, centers = read_data(state, zips, pops, centers)
 
-	pop_per_district = total_population / NUM_DISTRICTS
+	pop_per_district = total_population / num_districts
 
 	# Find all of the neighbors of each ZIP code
-	find_neighbors()
+	neighbors, zips = find_neighbors(neighbors, zips)
 
 	# Create the districts
-	create_districts(pop_per_district)
+	districts = create_districts(pop_per_district, districts, zips, neighbors, pops, centers)
 
 	print_dictionary(districts)
 
-	print_district_pops()
+	# Get the population of each district and print them
+	districts_pops = get_district_pops(districts, pops, districts_pops)
+	print_dictionary(districts_pops)
 
-	#outline_districts()
-	#print_dictionary(district_boundary)
-
-	#output_individuals()
-	#output_outlines() 
+	#output_individuals(districts, zips)
 
 	# Check to make sure sum of district populations equals the total population
 	print("Population per district:", pop_per_district)
 	print("Total population:", total_population)
+
+	z = json.dumps(zips)
+	d = json.dumps(districts)
+
+	return d, z
--- server.py
+++ server.py
@@ -1,26 +1,42 @@
 from back import run
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+districts = {}
+zips = {}
+state = ''
+num_districts = 0
 
 @app.route('/', methods=['GET','POST'])
 def index():
 
-    states = ['AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
+    states = ['AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+    numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
+    global state
+    global num_districts
 
     if request.method == 'POST':
         state = str(request.form.get('state_dropdown'))
         num_districts = int(str(request.form.get('number_dropdown')))
         print('State: ' + state)
         print('Num_districts: ' + str(num_districts))
-        run(state, num_districts)
+        return redirect(url_for('my_link'))
 
-    return render_template('template.html', states=states, numbers=numbers)
+    return render_template('dropdowns.html', states=states, numbers=numbers)
 
-@app.route('/generate/')
+@app.route('/generate/', methods=['GET','POST'])
 def my_link():
 
-    return 'Done'
+    global districts
+    global zips
+    global state
+    global num_districts
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+
+    districts, zips = run(state, num_districts)
+    return render_template('display.html', districts=districts, zips=zips, state=state, num_districts=num_districts)
 
 if __name__ == '__main__':
   app.run(debug=True)
