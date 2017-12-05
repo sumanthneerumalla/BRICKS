5d20ca3040069d8dfb3656fee408afd79e780d16
Zell86 <elliottzach86@gmail.com>
2017-12-05 15:54:39 -0500

    Updated flask server

--- server.py
+++ server.py
@@ -13,6 +13,22 @@ centers = {}
 @app.route('/', methods=['GET','POST'])
 def index():
 
+    if request.method == 'POST':
+        return redirect(url_for('my_link2'))
+
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/tutorial/')
+def tutorial():
+    return render_template('tutorial.html')
+
+@app.route('/list/', methods=['GET','POST'])
+def my_link2():
     states = ['Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
     numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
     global state
@@ -24,6 +40,7 @@ def index():
         return redirect(url_for('my_link'))
 
     return render_template('dropdowns.html', states=states, numbers=numbers)
+
 
 @app.route('/generate/', methods=['GET','POST'])
 def my_link():
@@ -37,10 +54,10 @@ def my_link():
     global centers
 
     if request.method == 'POST':
-        return redirect(url_for('index'))
+        return redirect(url_for('my_link2'))
 
     districts, zips, largest_zips, districts_pops, centers = run(state, num_districts)
     return render_template('display.html', districts=districts, zips=zips, state=state, num_districts=num_districts, largest_zips=largest_zips, districts_pops=districts_pops, centers=centers)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
